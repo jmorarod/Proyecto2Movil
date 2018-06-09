17646d31c498ab36d574d92a4c47476fddf0f0b5
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-09 14:00:00 -0600

    conectado con api de gmail

--- app/src/main/java/cr/ac/jmorarodic_itcr/proyecto2movil/InicioActivity.java
+++ app/src/main/java/cr/ac/jmorarodic_itcr/proyecto2movil/InicioActivity.java
@@ -1,12 +1,30 @@
 package cr.ac.jmorarodic_itcr.proyecto2movil;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class InicioActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class InicioActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+    Button login;
+    //private SignInButton login = findViewById(R.id.gmailLogin);
     public void onClickRegistrarse(View view){
         Intent intent = new Intent(this, RegistrarActivity.class);
         startActivity(intent);
@@ -19,5 +37,51 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        login = findViewById(R.id.button);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, 777);
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 777) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+
+        }
+    }
+
+    private void handleResult(GoogleSignInResult result) {
+        if(result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            Toast.makeText(this, account.getEmail(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
