87d224c8c6ead8b7204c9c1c2c6c6e917e5899d1
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-10 21:50:24 -0600

    crear anuncios

--- app/src/main/java/cr/ac/jmorarodic_itcr/proyecto2movil/AgregarAnuncioActivity.java
+++ app/src/main/java/cr/ac/jmorarodic_itcr/proyecto2movil/AgregarAnuncioActivity.java
@@ -1,21 +1,51 @@
 package cr.ac.jmorarodic_itcr.proyecto2movil;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cr.ac.jmorarodic_itcr.proyecto2movil.Models.Announcement;
+import cr.ac.jmorarodic_itcr.proyecto2movil.Models.SubcategoriaAnuncio;
+import cr.ac.jmorarodic_itcr.proyecto2movil.Models.Subcategory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
 
 public class AgregarAnuncioActivity extends AppCompatActivity {
 
@@ -23,12 +53,52 @@ public class AgregarAnuncioActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
     private Uri uri = null;
     private ImageView imgCell;
+    Map map = null;
+
+
+    ArrayList<String> subcategoriaNombres;
+    ArrayList<SubcategoriaAnuncio> subcategorias;
+    private Retrofit retrofit;
+    FreembeService service;
+
+    ArrayAdapter<String> spinnerAdapter;
+    Spinner spinner;
+
+    private EditText txtTitle;
+    private EditText txtDescription;
+    private EditText txtPrice;
+    private EditText txtLocation;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_anuncio);
         imgCell = findViewById(R.id.imgAnuncio);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://freembe.herokuapp.com/api/")  // Este es el url base del api
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = retrofit.create(FreembeService.class);
+
+        spinner = findViewById(R.id.spinnerSubcategorias);
+        subcategoriaNombres = new ArrayList<>();
+        subcategorias = new ArrayList<>();
+
+        spinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, subcategoriaNombres);
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrecio);
+        txtLocation = findViewById(R.id.txtLocation);
+
+
+        obtenerSubcategorias();
+
 
     }
 
@@ -87,6 +157,118 @@ public class AgregarAnuncioActivity extends AppCompatActivity {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void obtenerSubcategorias() {
+
+
+        Call<List<Subcategory>> subcategoryResultCall = service.obtenerSubcategorias("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1MjkxMDI1OTZ9.Ch3I8ScU927ZayFJK3jUCg0OZCJBB9VZvheCarHacjY");
+
+        subcategoryResultCall.enqueue(new Callback<List<Subcategory>>() {
+            @Override
+            public void onResponse(Call<List<Subcategory>> call, Response<List<Subcategory>> response) {
+                if(response.isSuccessful()) {
+                    for(Subcategory s: response.body()) {
+                        SubcategoriaAnuncio sa = new SubcategoriaAnuncio(s.getId(), s.getName());
+                        subcategorias.add(sa);
+                    }
+
+                    for(int i = subcategorias.size()-1; i>=0; i--){
+                        subcategoriaNombres.add(subcategorias.get(i).getNombre());
+                    }
+                    spinner.setAdapter(spinnerAdapter);
+                }
+                else {
+                    Log.e("categories_error_body: ", response.errorBody().toString());
+                }
+            }
+
+
+            @Override
+            public void onFailure(Call<List<Subcategory>> call, Throwable t) {
+                Log.e("categories_failure: ", t.getMessage());
+
+            }
+        });
+    }
+
+
+    public void subirAnuncio(View view) {
+        ImageUploadTask imageUploadTask = new ImageUploadTask();
+        imageUploadTask.execute(uri);
+    }
+
+
+    public static String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+
+
+
+
+    class ImageUploadTask extends AsyncTask<Uri,Void,Bitmap> {
+        File file;
+        FileInputStream fileInputStream;
+        @Override
+        protected Bitmap doInBackground(Uri... urls) {
+
+            Map config = new HashMap();
+            config.put("cloud_name", "poppycloud");
+            config.put("api_key", "328358331617938");
+            config.put("api_secret", "z-7k70XpvP1dl1ZdiqVF0olXp7A");
+
+            Cloudinary cloudinary = new Cloudinary(config);
+
+            file = new File(String.valueOf(urls[0]));
+            try {
+                fileInputStream = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                Log.i("URI",urls[0].getPath());
+                String path = getRealPathFromUri(getApplicationContext(),urls[0]);
+                Log.i("Path",path);
+                map = cloudinary.uploader().upload(path, ObjectUtils.emptyMap() );
+
+                String title = txtTitle.getText().toString();
+                String description = txtDescription.getText().toString();
+                float price = Float.parseFloat(txtPrice.getText().toString());
+                String location = txtLocation.getText().toString();
+                int subcategory = subcategorias.get(spinner.getSelectedItemPosition()).getId();
+
+
+                Call<Announcement> announcementCall = service.crearAnuncio("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1MjkxMDI1OTZ9.Ch3I8ScU927ZayFJK3jUCg0OZCJBB9VZvheCarHacjY", title, description, price, (String) map.get("secure_url"), 1, 0.0f, 0.0f, 1, location);
+                announcementCall.enqueue(new Callback<Announcement>() {
+                    @Override
+                    public void onResponse(Call<Announcement> call, Response<Announcement> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Announcement> call, Throwable t) {
+
+                    }
+                });
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
---
+++ app/src/main/java/cr/ac/jmorarodic_itcr/proyecto2movil/Models/SubcategoriaAnuncio.java
@@ -0,0 +1,28 @@
+package cr.ac.jmorarodic_itcr.proyecto2movil.Models;
+
+public class SubcategoriaAnuncio {
+
+    private int id;
+    private String nombre;
+
+    public SubcategoriaAnuncio(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
